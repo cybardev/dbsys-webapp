a99c0c5ec64f31b34531379a663bdba407adf02e
cybardev <sheikh@cybar.dev>
2024-03-30 14:05:07 -0300

    re: remove redundant comment stubs

--- dbutils/j2sql_parts.sh
+++ dbutils/j2sql_parts.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-#
+
 #  Write below your own credentials
 user="uXY"
 pass="XYZ"
 db="uXY"
-#
+
 echo
-#
+
 mongoimport -d "$db" -c parts -u "$user" --password="$pass" --type="json" --file="parts_100.json"
 mongoexport -d "$db" -c parts -u "$user" -p "$pass" --type=csv --fields "_id,price,description" | tail -n +2 >parts.csv
-#
+
 echo "source parts_table.sql;" | mysql -u "$user" --password="$pass" "$db"
 cat parts.csv | tr "," "\t" >parts.tsv
 echo "load data local infile 'parts.tsv' into table parts" | mysql $db -u $user --password="$pass"
