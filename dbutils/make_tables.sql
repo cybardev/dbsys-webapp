8a12e5cba449d9ff779fadcb79c8038b0c0babda
cybardev <sheikh@cybar.dev>
2024-04-08 22:10:40 -0300

    fix: place not null constraint

--- dbutils/make_tables.sql
+++ dbutils/make_tables.sql
@@ -1,30 +1,30 @@
 -- Suppliers Table
 CREATE TABLE IF NOT EXISTS suppliers (
     supplier_id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(255),
-    email VARCHAR(255)
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
 -- Telephone Numbers Table for Suppliers
 CREATE TABLE IF NOT EXISTS suppliers_telephone (
-    number VARCHAR(20) PRIMARY KEY,
-    supplier_id INT,
+    number VARCHAR(20) PRIMARY KEY NOT NULL,
+    supplier_id INT NOT NULL,
     FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id)
 );
 
 -- Orders Table
 CREATE TABLE IF NOT EXISTS orders (
-    order_id INT PRIMARY KEY,
-    order_date DATE,
-    supplier_id INT,
+    order_id INT PRIMARY KEY NOT NULL,
+    order_date DATE NOT NULL,
+    supplier_id INT NOT NULL,
     FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id)
 );
 
 -- Order Part Relationship Table
 CREATE TABLE IF NOT EXISTS order_parts (
-    order_id INT,
-    part_id INT,
-    quantity INT,
+    order_id INT NOT NULL,
+    part_id INT NOT NULL,
+    quantity INT NOT NULL,
     PRIMARY KEY (order_id, part_id),
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (part_id) REFERENCES parts(_id)
