39aa7212de859b8e0560a6b4ccf2246879f91fab
cybardev <sheikh@cybar.dev>
2024-04-13 23:33:36 -0300

    feat: create WSGI app for Render

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
 # module declaration script
+
+import os
+
+from app.app import app_factory
+
+
+render = app_factory(
+    "localhost",
+    os.environ["DB_USER"],
+    os.environ["DB_PASS"],
+    os.environ["DB_NAME"],
+)
