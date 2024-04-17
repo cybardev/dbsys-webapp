1c0297fefb222c0c6f3d2bc3c1090e5463511ba8
cybardev <sheikh@cybar.dev>
2024-04-17 00:35:03 -0300

    feat: create DB tables if they don't exist

--- app/utils.py
+++ app/utils.py
@@ -4,11 +4,22 @@ Utility functions
 
 import os
 
+import psycopg
+
 from app.app import app_factory
 
 
 def render():
-    return app_factory(os.environ["DB_URL"])
+    DB_URL = os.environ["DB_URL"]
+
+    # create DB tables if they don't exist
+    with psycopg.connect(DB_URL) as db:
+        with db.cursor() as cursor:
+            for fname in ("parts_table", "make_tables"):
+                with open(f"./dbutils/sql/{fname}.sql") as file:
+                    cursor.execute(file.read())
+
+    return app_factory(DB_URL)
 
 
 def sanitize_input(s: str) -> str:
